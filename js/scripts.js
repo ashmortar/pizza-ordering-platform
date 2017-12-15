e62d0d0dc20938957c3365b88af3e890eb54e4f4
Aaron Ross <ashmortar@gmail.com>
2017-12-15 09:02:27 -0800

    create a pizza constructor that accepts size as a property

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//CONSTRUCTORS AND PROTOTYPES ---------------------------------
+
+function Pizza(size) {
+  this.size = size;
+}
+
+
+
+//LOOSE HELPER FUNCTIONS ---------------------------------------
+
+
+
+
+
+//GLOBAL VARIABLES ---------------------------------------------
+
+
+
+
+
+
+//FRONT END BELOW THIS LINE ------------------------------------
