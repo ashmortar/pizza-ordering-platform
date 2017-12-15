a7c71b7598f6d2aa762d811cb1893f88d797485c
Aaron Ross <ashmortar@gmail.com>
2017-12-15 09:13:31 -0800

    add cost prototype that calculates the pice of a pizza and stores that as a property of that pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,24 @@
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
+  this.price = 0.00;
 }
 
+Pizza.prototype.cost = function() {
+  if (this.size === "small") {
+    this.price = 5.00;
+  } else if (this.size === "medium") {
+    this.price = 10.00;
+  } else if (this.size === "large") {
+    this.price = 15.00;
+  } else {
+    console.log("this is not a pizza size")
+  }
+  for (var i = 0; i < this.toppings.length; i++) {
+    this.price += 0.50;
+  }
+  console.log("cost: $" + this.price);
+}
 
 
 //LOOSE HELPER FUNCTIONS ---------------------------------------
